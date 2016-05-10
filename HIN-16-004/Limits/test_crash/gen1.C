3ac8fec4e3fac54d1c255b66a04ac0d625f328bd
Emilien Chapon <emilien.chapon@cern.ch>
2016-05-10 19:33:38 +0200

    Add test_crash

---
+++ HIN-16-004/Limits/test_crash/gen1.C
@@ -0,0 +1,39 @@
+#ifndef __CINT__
+#include "RooGlobalFunc.h"
+#endif
+#include "RooRealVar.h"
+#include "RooDataSet.h"
+#include "RooGaussian.h"
+#include "TCanvas.h"
+#include "RooPlot.h"
+#include "TAxis.h"
+using namespace RooFit ;
+
+void gen1(const char* output, const char* suffix) {
+  // S e t u p   m o d e l 
+  // ---------------------
+
+   gRandom = new TRandom();
+   gRandom->SetSeed(0);
+   RooWorkspace *ws = new RooWorkspace("workspace");
+  // Declare variables x,mean,sigma with associated name, title, initial value and allowed range
+  ws->factory("invMass[2,5]");
+  RooRealVar *invMass = ws->var("invMass");
+  ws->factory(Form("Gaussian::sig_nonorm_%s(invMass,m0_%s[3,2.5,3.5],sigma_%s[0.1,0.,0.5])",suffix,suffix,suffix));
+  ws->factory(Form("RooExtendPdf::sig_%s(sig_nonorm_%s,Nsig_%s[1e4,-1e5,1e5])",suffix,suffix,suffix));
+  ws->factory(Form("Chebychev::bkg_nonorm_%s(invMass,{lambda0_%s[0.1,-1.5,1.5],lambda1_%s[0.1,-1.5,1.5]})",suffix,suffix,suffix));
+  ws->factory(Form("RooExtendPdf::bkg_%s(bkg_nonorm_%s,Nbkg_%s[1e4,-1e5,1e5])",suffix,suffix,suffix));
+  // ws->factory(Form("SUM::tot_%s( sig_%s,  bkg_%s)",suffix,suffix,suffix));
+  // RooAbsPdf *thepdf = ws->pdf(Form("tot_%s",suffix));
+  RooAbsPdf *thepdf = new RooAddPdf(Form("tot_%s",suffix), Form("tot_%s",suffix), RooArgList(*ws->pdf(Form("sig_%s",suffix)), *ws->pdf(Form("bkg_%s",suffix))));
+  ws->import(*thepdf);
+
+  // G e n e r a t e   e v e n t s 
+  // -----------------------------
+
+  // Generate a dataset of 1000 events in x from gauss
+  RooDataSet* data = thepdf->generate(*invMass,2e4,Name(Form("data_%s",suffix))) ;  
+  ws->import(*data);
+
+  ws->writeToFile(output);
+}
