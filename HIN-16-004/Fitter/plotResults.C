cea7cdb38904d36b177b12998d0128fc132a31b5
Emilien Chapon <emilien.chapon@cern.ch>
2016-05-26 19:12:50 +0200

    Update NP contamination syst

--- HIN-16-004/Fitter/plotResults.C
+++ HIN-16-004/Fitter/plotResults.C
@@ -191,6 +191,7 @@ void plot(vector<anabin> thecats, string xaxis, string outputDir) {
       syst_PP = combineSyst(all_PP,"statsyst_PP");
    }
    map<anabin, syst> syst_PbPb = readSyst_all("PbPb");
+   map<anabin, syst> syst_PbPb_NP_add = readSyst_all("Systematics/csv/syst_PbPb_bhad_add.csv");
 
    // make TGraphAsymmErrors
    int cnt=0;
@@ -235,7 +236,6 @@ void plot(vector<anabin> thecats, string xaxis, string outputDir) {
             // exsyst = !isMB ? 5 : 5./(1.-xfrac);
             exsyst = exl;
             eysyst = syst_PbPb[thebin].value; // only PbPb syst: the PP one will go to a dedicated box
-            // also add
          }
          y = theVarsBinned[*it][i]->getVal();
          if (fiterrors) {
@@ -252,7 +252,10 @@ void plot(vector<anabin> thecats, string xaxis, string outputDir) {
                eyh = theVarsBinned[*it][i]->getErrorHi();
             }
          }
+
          eysyst = y*eysyst;
+         eysyst += syst_PbPb_NP_add[thebin].value; // add the additive part of the NP contamination syst
+
          theGraphs[*it]->SetPoint(i,x,y);
          theGraphs[*it]->SetPointError(i,exl,exh,eyl,eyh);
          theGraphs_syst[*it]->SetPoint(i,x,y);
