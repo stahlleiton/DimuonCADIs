71a4016c1106833995dd19c3a89b9c56002614f9
Emilien Chapon <emilien.chapon@cern.ch>
2016-06-20 10:53:00 +0200

    Limits using RooStats::FeldmanCousins

---
+++ HIN-16-004/Limits/computeLimits_FC.C
@@ -0,0 +1,112 @@
+#ifndef computeLimits_C
+#define computeLimits_C
+
+#include "TString.h"
+
+#include <vector>
+#include <sstream>
+
+#include "../Fitter/Macros/Utilities/resultUtils.h"
+#include "runLimit_RaaNS_Workspace.C"
+#include "limits.h"
+
+using namespace std;
+
+const bool usebatch=true;
+const char* asymLimitsFileName = "csv/cLimits_950_nominal_all_newSystAlphaFix_20160614_CL95_Asym_Asym_wSys.csv";
+
+void computeLimits_FC(
+                   const char* ACTag, // ACTag: where to look for combined workspaces in Limits/CombinedWorkspaces/
+                   bool doSyst= false,
+                   double CL=0.95 // Confidence Level for limits computation
+)
+// define calc type and test stat type:
+//
+// calculatorType = 0 Freq calculator
+// calculatorType = 1 Hybrid calculator
+// calculatorType = 2 Asymptotic calculator
+// calculatorType = 3 Asymptotic calculator using nominal Asimov data sets (not using fitted parameter values but nominal ones)
+//
+// testStatType = 0 LEP
+//              = 1 Tevatron
+//              = 2 Profile Likelihood two sided
+//              = 3 Profile Likelihood one sided (i.e. = 0 if mu < mu_hat)
+//              = 4 Profile Likelihood signed ( pll = -pll if mu < mu_hat)
+//              = 5 Max Likelihood Estimate as test statistic
+//              = 6 Number of observed event as test statistic
+//
+// 0,3,true for frequentist CLs; 2,3,true for asymptotic CLs; 0,2,false for FC
+
+{
+  // list of files
+  vector<TString> theFiles = combFileList(ACTag, doSyst ? "wSyst" : "woSyst");
+  if ( theFiles.empty() )
+  {
+    cout << "# [Error]: No combined workspaces found" << endl;
+    return;
+  }
+
+  // asymptotic limits
+  map<anabin, limits> asymlims = readLimits(asymLimitsFileName);
+  
+  // File to save results
+  ostringstream strs;
+  strs << CL;
+  string str = strs.str();
+  TString sCL(str);
+  sCL.Remove(0,sCL.First('.')+1);
+  string limitsFileName = string("csv/") + "cLimits_" + string(sCL) + "_" + string(ACTag);
+  if ( doSyst ) limitsFileName = limitsFileName + "_wSyst.csv";
+  else limitsFileName = limitsFileName + "_woSyst.csv";
+  
+  ofstream file(limitsFileName.c_str());
+  file << CL << endl;
+  
+  // Confidence interval computation
+  int cnt=1;
+  for (vector<TString>::const_iterator it=theFiles.begin(); it!=theFiles.end(); it++)
+  {
+    cout << ">>>>>>> Computing " << CL*100 << "% " << "limits for analysis bin " << cnt << endl;
+    cout << "Using combined PbPb-PP workspace " << cnt << " / " << theFiles.size() << ": " << *it << endl;
+
+    anabin thebin = binFromFile(*it);
+    double poimin = 0.95*asymlims[thebin].val.second;
+    double poimax = 1.05*asymlims[thebin].val.second;
+    // RooRealVar *thePoi = poiFromFile(it->Data(),"_PbPbvsPP","RFrac2Svs1S");
+    // double poimin = 0.9*(thePoi->getVal()+thePoi->getError());
+    // double poimax = 1.1*(thePoi->getVal()+thePoi->getError());
+    if (!usebatch) {
+       pair<double,double> lims = runLimit_RaaNS_Workspace(*it, "RFrac2Svs1S_PbPbvsPP", "simPdf", "workspace", "dOS_DATA", ACTag, CL, poimin, poimax);
+
+
+       file << thebin.rapbin().low() << ", " << thebin.rapbin().high() << ", "
+          << thebin.ptbin().low() << ", " << thebin.ptbin().high() << ", "
+          << thebin.centbin().low() << ", " << thebin.centbin().high() << ", "
+          << lims.first << ", " << lims.second << endl;
+    } else {
+       TString exports;
+       exports += Form("export it=%s; ",it->Data());
+       exports += Form("export ACTag=%s; ",ACTag);
+       exports += Form("export CL=%f; ",CL);
+       exports += Form("export poimin=%f; ",poimin);
+       exports += Form("export poimax=%f; ",poimax);
+       exports += Form("export pwd_=%s; ", gSystem->pwd());
+       TString command("qsub -k oe -q cms@llrt3 -l nodes=1:ppn=23 ");
+       command += Form("-N limits_bin%i ",cnt);
+       command += "-V ";
+       command += Form("-o %s ", gSystem->pwd());
+       command += Form("-v it,ACTag,CL,poimin,poimax,pwd_ ");
+       command += "runbatch_limits_FC.sh";
+       TString command_full = exports + command;
+       cout << command_full.Data() << endl;
+       system(command_full.Data());
+       system("sleep 1");
+    }
+    
+    cnt++;
+  } // loop on the files
+  file.close();
+  cout << "Closed " << limitsFileName << endl << endl;
+}
+
+#endif // #ifndef computeLimits_C
---
+++ HIN-16-004/Limits/runLimit_RaaNS_Workspace_FC.C
@@ -0,0 +1,103 @@
+#include "RooStats/ProfileLikelihoodCalculator.h"
+#include "RooStats/LikelihoodInterval.h"
+#include "RooStats/LikelihoodIntervalPlot.h"
+#include "RooStats/FeldmanCousins.h"
+#include "RooStats/ProfileLikelihoodTestStat.h"
+#include "TLatex.h"
+#include "TFile.h"
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+int nworkers = 0;                        // number of worker for ProofLite (default use all available cores)
+int nPointsToScan = 20;
+double additionalToysFac = 0.5;
+
+pair<double,double> runLimit_RaaNS_Workspace_FC(const char *filename="TRIAL.root", const char *poiname="raa3", const char *pdfname="joint", const char *wsname="wcombo", const char* dataname="dOS_DATA", const char* ACTag="", double CI = 0.95, double poimin=0, double poimax=10);
+
+// #include "StandardHypoTestInvDemo.C"
+
+using namespace std;
+using namespace RooStats;
+
+pair<double,double> runLimit_RaaNS_Workspace_FC(const char *filename, const char *poiname, const char *pdfname, const char *wsname, const char* dataname, const char* ACTag, double CI, double poimin, double poimax)
+{
+   // RooMsgService::instance().addStream(RooFit::MsgLevel::DEBUG); 
+   RooRealVar *theVar; RooDataSet *data; RooAbsPdf *pdf;
+   TFile *f = new TFile(filename) ;
+  
+   // Open text file to write results
+   ofstream of("results.txt");
+
+   // Retrieve workspace from file
+   RooWorkspace* ws = (RooWorkspace*) f->Get(wsname);
+   RooStats::ModelConfig *sbHypo = (RooStats::ModelConfig*) ws->obj("SbHypo");
+   RooStats::ModelConfig *bHypo = (RooStats::ModelConfig*) ws->obj("BHypo");
+   cout << "observables: " << sbHypo->GetObservables()->getSize() << endl;
+   theVar = ws->var(poiname);
+   // if (!theVar) theVar = ws->function(poiname);
+   pdf = ws->pdf(pdfname);
+   data =(RooDataSet *) ws->data(dataname);
+
+   // Print structure of composite p.d.f.
+   pdf->Print("t") ;
+
+   of << "Scanning " << filename << " using the POI " << poiname << ", the pdf " << pdfname << " and the workspace " << wsname << " with CL " << CI << endl;
+   ws->loadSnapshot("SbHypo_poiAndNuisance_snapshot");
+   of << poiname << " " << theVar->getVal() << endl;
+
+   FeldmanCousins fc(*data,*sbHypo);
+   fc.SetConfidenceLevel(CI);
+   fc.AdditionalNToysFactor(additionalToysFac); // improve sampling that defines confidence belt
+   //  fc.UseAdaptiveSampling(true); // speed it up a bit, but don't use for expectd limits
+   RooDataSet poivals("poivals","poivals",RooArgSet(*theVar));
+   double oldval = theVar->getVal();
+   double dvar = (poimax-poimin)/nPointsToScan;
+   cout << "TEST " << poimin << " " << poimax << endl;
+   for (int i=0; i<nPointsToScan; i++) {
+      theVar->setVal(poimin+i*dvar);
+      poivals.add(RooArgSet(*theVar));
+   }
+   theVar->setVal(oldval);
+   // fc.SetNBins(nPointsToScan); // set how many points per parameter of interest to scan
+   // fc.SetPOIPointsToTest(poivals);
+   // if (!sbHypo->GetNuisanceParameters()) fc.SetParameterPointsToTest(poivals);
+   fc.SetPOIPointsToTest(poivals);
+   fc.CreateConfBelt(true); // save the information in the belt for plotting
+
+   ToyMCSampler*  toymcsampler = (ToyMCSampler*) fc.GetTestStatSampler();
+   ProfileLikelihoodTestStat* testStat = dynamic_cast<ProfileLikelihoodTestStat*>(toymcsampler->GetTestStatistic());
+
+   if(!sbHypo->GetPdf()->canBeExtended()){
+      if(data->numEntries()==1)
+         fc.FluctuateNumDataEntries(false);
+      else
+         cout <<"Not sure what to do about this model" <<endl;
+   }
+
+   ProofConfig pc(*ws, nworkers, "",false);
+   toymcsampler->SetProofConfig(&pc); // enable proof
+
+   if(sbHypo->GetGlobalObservables()){
+      cout << "will use global observables for unconditional ensemble"<<endl;
+      sbHypo->GetGlobalObservables()->Print();
+      toymcsampler->SetGlobalObservables(*sbHypo->GetGlobalObservables());
+   }
+
+
+   // Now get the interval
+   PointSetInterval* interval = fc.GetInterval();
+   ConfidenceBelt* belt = fc.GetConfidenceBelt();
+
+
+   pair<double,double> lims;
+   lims.first = interval->LowerLimit(*theVar);
+   if (lims.first == poimin) lims.first = -1e30;
+   lims.second = interval->UpperLimit(*theVar);
+   if (lims.second == poimax) lims.second = 1e30;
+
+   cout << "The limits are [" << lims.first << ", " << lims.second << "]" << endl;
+  
+  return lims;
+}
