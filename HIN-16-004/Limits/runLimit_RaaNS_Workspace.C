09c976b2165123048370bda99da56f2533c04dca
Emilien Chapon <emilien.chapon@cern.ch>
2016-07-13 14:42:17 +0200

    Change strategy fro Frequentist limits

--- HIN-16-004/Limits/runLimit_RaaNS_Workspace.C
+++ HIN-16-004/Limits/runLimit_RaaNS_Workspace.C
@@ -7,12 +7,12 @@
 using namespace RooStats;
 using namespace std;
 
-pair<double,double> runLimit_RaaNS_Workspace(const char *filename="TRIAL.root", const char *poiname="raa3", const char *pdfname="joint", const char *wsname="wcombo", const char* dataname="dOS_DATA", const char* ACTag="", double CI = 0.95, int calculatorType = 2, int testStatType = 2,  bool useCLs = false);
+pair<double,double> runLimit_RaaNS_Workspace(const char *filename="TRIAL.root", const char *poiname="raa3", const char *pdfname="joint", const char *wsname="wcombo", const char* dataname="dOS_DATA", const char* ACTag="", double CI = 0.95, int calculatorType = 2, int testStatType = 2,  bool useCLs = false, double poival=-1, int rndseed=-1);
 
 #include "StandardHypoTestInvDemo.C"
 
 
-pair<double,double> runLimit_RaaNS_Workspace(const char *filename, const char *poiname, const char *pdfname, const char *wsname, const char* dataname, const char* ACTag, double CI, int calculatorType, int testStatType,  bool useCLs)
+pair<double,double> runLimit_RaaNS_Workspace(const char *filename, const char *poiname, const char *pdfname, const char *wsname, const char* dataname, const char* ACTag, double CI, int calculatorType, int testStatType,  bool useCLs, double poival, int rndseed)
 {
    // RooMsgService::instance().addStream(RooFit::MsgLevel::DEBUG); 
    RooRealVar *theVar; RooDataSet *data; RooAbsPdf *pdf;
@@ -38,54 +38,60 @@ pair<double,double> runLimit_RaaNS_Workspace(const char *filename, const char *p
    ws->loadSnapshot("SbHypo_poiAndNuisance_snapshot");
    of << poiname << " " << theVar->getVal() << endl;
 
-   // get a first estimate of where to look
+   double poimin,poimax;
+   if (calculatorType==0 && rndseed>0) {
+      poimin=poival;
+      poimax=poival;
+   } else {
+      // get a first estimate of where to look
 
-   // pdf->fitTo(*data, Offset(kTRUE), Extended(kTRUE), NumCPU(2), Range("MassWindow"), Save(), Minimizer("Minuit2","Migrad"));
-   // pdf->fitTo(*data, Extended(kTRUE), Offset(kTRUE));
-   ProfileLikelihoodCalculator pl(*data,*sbHypo,1.-CI);
-   pl.SetConfidenceLevel(CI); 
-   int ci = 100*CI;
-   LikelihoodInterval* interval = pl.GetInterval();
-   LikelihoodIntervalPlot plot(interval);
-   TCanvas c4; c4.cd(); 
-   plot.SetRange(0.,1.,0.05,3.);
-   // plot.Draw();
-   TLatex latexCI;
-   latexCI.SetNDC();
-   latexCI.SetTextSize(0.035);
-   latexCI.DrawLatex(0.5,1.-0.05*2,Form("%s %d %s C.I.",poiname,ci,"%"));
-   latexCI.DrawLatex(0.5,1.-0.05*3,Form("Upper limit: %f",interval->UpperLimit(*theVar)));
-   latexCI.DrawLatex(0.5,1.-0.05*4,Form("Lower limit: %f",interval->LowerLimit(*theVar)));
-   TString intrvlName = theVar->GetTitle();
-   // print out the iterval on the Parameter of Interest
-   cout <<endl<< CI <<"\% interval on " <<theVar->GetName()<<" is : ["<<
-      interval->LowerLimit(*theVar) << ", "<<
-      interval->UpperLimit(*theVar) << "] "<<endl;
-   pair<double, double> CnfdncIntrvl;
-   CnfdncIntrvl.first  = interval->LowerLimit(*theVar);
-   CnfdncIntrvl.second = interval->UpperLimit(*theVar);
-   c4.SaveAs("ULtest.pdf");
+      // pdf->fitTo(*data, Offset(kTRUE), Extended(kTRUE), NumCPU(2), Range("MassWindow"), Save(), Minimizer("Minuit2","Migrad"));
+      // pdf->fitTo(*data, Extended(kTRUE), Offset(kTRUE));
+      ProfileLikelihoodCalculator pl(*data,*sbHypo,1.-CI);
+      pl.SetConfidenceLevel(CI); 
+      int ci = 100*CI;
+      LikelihoodInterval* interval = pl.GetInterval();
+      LikelihoodIntervalPlot plot(interval);
+      TCanvas c4; c4.cd(); 
+      plot.SetRange(0.,1.,0.05,3.);
+      // plot.Draw();
+      TLatex latexCI;
+      latexCI.SetNDC();
+      latexCI.SetTextSize(0.035);
+      latexCI.DrawLatex(0.5,1.-0.05*2,Form("%s %d %s C.I.",poiname,ci,"%"));
+      latexCI.DrawLatex(0.5,1.-0.05*3,Form("Upper limit: %f",interval->UpperLimit(*theVar)));
+      latexCI.DrawLatex(0.5,1.-0.05*4,Form("Lower limit: %f",interval->LowerLimit(*theVar)));
+      TString intrvlName = theVar->GetTitle();
+      // print out the iterval on the Parameter of Interest
+      cout <<endl<< CI <<"\% interval on " <<theVar->GetName()<<" is : ["<<
+         interval->LowerLimit(*theVar) << ", "<<
+         interval->UpperLimit(*theVar) << "] "<<endl;
+      pair<double, double> CnfdncIntrvl;
+      CnfdncIntrvl.first  = interval->LowerLimit(*theVar);
+      CnfdncIntrvl.second = interval->UpperLimit(*theVar);
+      c4.SaveAs("ULtest.pdf");
 
-   // pair<double, double> CnfdncIntrvl;
-   // CnfdncIntrvl.first  = theVar->getVal()+theVar->getErrorLo();
-   // CnfdncIntrvl.second = theVar->getVal()+theVar->getErrorHi();
+      // pair<double, double> CnfdncIntrvl;
+      // CnfdncIntrvl.first  = theVar->getVal()+theVar->getErrorLo();
+      // CnfdncIntrvl.second = theVar->getVal()+theVar->getErrorHi();
 
 
-   of << 100.*CI << "% CL confidence interval from a likelihood scan on " << poiname << endl;
-   of << CnfdncIntrvl.first << " " << CnfdncIntrvl.second << endl;
+      of << 100.*CI << "% CL confidence interval from a likelihood scan on " << poiname << endl;
+      of << CnfdncIntrvl.first << " " << CnfdncIntrvl.second << endl;
 
-   // now let's move to the hypo test inverter
-   /////////////////////////////////////////////////////////////
-   // Now get the POI for convenience
-   // you may want to adjust the range of your POI
-   ////////////////////////////////////////////////////////////
-   RooRealVar* firstPOI = (RooRealVar*) sbHypo->GetParametersOfInterest()->first();
-   double poimin = 0.5*max((double) 0.,CnfdncIntrvl.first);
-   double poimax = 1.5*CnfdncIntrvl.second;
-   cout << "Will scan between " << poimin << " and " << poimax << endl;
-   // firstPOI->setMin(poimin);
-   // firstPOI->setMax(poimax);
-   cout << "POI defined between" << firstPOI->getMin() << " and " << firstPOI->getMax() << endl;
+      // now let's move to the hypo test inverter
+      /////////////////////////////////////////////////////////////
+      // Now get the POI for convenience
+      // you may want to adjust the range of your POI
+      ////////////////////////////////////////////////////////////
+      RooRealVar* firstPOI = (RooRealVar*) sbHypo->GetParametersOfInterest()->first();
+      poimin = 0.5*max((double) 0.,CnfdncIntrvl.first);
+      poimax = 1.5*CnfdncIntrvl.second;
+      cout << "Will scan between " << poimin << " and " << poimax << endl;
+      // firstPOI->setMin(poimin);
+      // firstPOI->setMax(poimax);
+      cout << "POI defined between" << firstPOI->getMin() << " and " << firstPOI->getMax() << endl;
+   }
 
    // actually run the hypo test inverter
 
@@ -115,6 +121,14 @@ pair<double,double> runLimit_RaaNS_Workspace(const char *filename, const char *p
    TStopwatch tw; 
    tw.Start();
 
+   int npoints=20;
+   int ntoys=1000;
+
+   if (calculatorType==0 && rndseed>0) {
+      npoints=1;
+      ntoys=20;
+   }
+
    HypoTestInverterResult* r = StandardHypoTestInvDemo(f->GetName(),
          wsname,
          sbHypo,
@@ -123,14 +137,15 @@ pair<double,double> runLimit_RaaNS_Workspace(const char *filename, const char *p
          calculatorType,
          testStatType, 
          useCLs,
-         20,
+         npoints,
          poimin,
          poimax,
-         1000,
+         ntoys,
          false,
          0,
          CI,
-         ACTag);
+         ACTag,
+         rndseed);
 
    pair<double,double> lims;
    lims.first = r->LowerLimit();
