76915eed5a7c9b107e7df1b51a6b2d2a0a73763a
Andre Stahl <stahlleiton@gmail.com>
2017-04-03 17:29:22 +0200

    modified fitter.C to automatecally do Peripheral fits if the inputWorkDirName has Peri in its name

--- HIN-16-004/Fitter/fitter.C
+++ HIN-16-004/Fitter/fitter.C
@@ -73,17 +73,18 @@ void fitter(
   binWidth["CTAURES"]  = 0.25;
   binWidth["CTAUSB"]   = 0.100;
 
+  if (workDirName.find("Peri")!=std::string::npos) { usePeriPD = true; }
+
   map<string, string> inputFitDir;
-  inputFitDir["MASS"]     = string("/afs/cern.ch/work/j/jmartinb/public/JpsiRAA/Output/") + workDirName + "/";
+  inputFitDir["MASS"]     = string("/afs/cern.ch/work/j/jmartinb/public/JpsiRAA/Output/") + (usePeriPD ? "DataFitsPeri/" :  "DataFitsCent/");
   inputFitDir["CTAUERR"]  = string("/afs/cern.ch/user/a/anstahll/work/public/RAAFITS/NOMINAL/") + (usePeriPD ? "DataFitsPeri/" :  "DataFitsCent/");
   inputFitDir["CTAUTRUE"] = string("/afs/cern.ch/user/v/vabdulla/public/PbPb2015/DataFits/");
   inputFitDir["CTAURECO"] = string("");
-  // inputFitDir["CTAURES"]  = string("/home/llr/cms/abdullah/RAA/DimuonCADIs/HIN-16-004/Fitter/Output/") + (usePeriPD ? "DataFitsPeri/" :  "DataFitsCent/");
-  inputFitDir["CTAURES"]  = string("");//string("/afs/cern.ch/user/v/vabdulla/public/PbPb2015/") + (usePeriPD ? "DataFitsPeri_NP/" :  "DataFitsCent_NP/");
-  inputFitDir["CTAUSB"]   = string("/home/llr/cms/stahl/RAA_Analysis/NORMAL/DimuonCADIs/HIN-16-004/Fitter/Output/") + workDirName + "/";
+  inputFitDir["CTAURES"]  = string("");//string("/afs/cern.ch/user/v/vabdulla/public/PbPb2015/")  + (usePeriPD ? "DataFitsPeri/" :  "DataFitsCent/");//+ (usePeriPD ? "DataFitsPeri_NP/" :  "DataFitsCent_NP/");
+  inputFitDir["CTAUSB"]   = string("/home/llr/cms/stahl/RAA_Analysis/NORMAL/DimuonCADIs/HIN-16-004/Fitter/Output/") + (usePeriPD ? "DataFitsPeri/" :  "DataFitsCent/");
   
   map<string, string> inputInitialFilesDir;
-  inputInitialFilesDir["MASS"]     = string("/afs/cern.ch/work/j/jmartinb/public/JpsiRAA/Input/") + workDirName + "/";
+  inputInitialFilesDir["MASS"]     = string("/afs/cern.ch/work/j/jmartinb/public/JpsiRAA/Input/") + (usePeriPD ? "DataFitsPeri/" :  "DataFitsCent/");
   inputInitialFilesDir["CTAUTRUE"] = "";
   inputInitialFilesDir["CTAURECO"] = "";
   inputInitialFilesDir["CTAURES"]  = string("/afs/cern.ch/user/v/vabdulla/public/PbPb2015/Input/") + (usePeriPD ? "DataFitsPeri/" :  "DataFitsCent/");
