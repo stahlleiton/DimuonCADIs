d350d1cbdc927dc53ad7449ab17f6851d2736ba7
Javier Martin <javier.martin-blanco@cern.ch>
2017-06-08 20:14:39 +0200

    small change to handle 004/025

--- HIN-16-004/Fitter/results2tree_all.C
+++ HIN-16-004/Fitter/results2tree_all.C
@@ -12,45 +12,46 @@ using namespace std;
 void results2tree_all(bool is16004 = false)
 {
   TString app("");
-  if (is16004) app = "16004_";
+  if (is16004) app = "16004";
+  else app = "16025";
   
   TString workdir("");
   // Nominal
-  workdir = Form("DataFits_%s2D_2CB_polBkg_nominal",app.Data());
+  workdir = Form("DataFits_%s_2D_2CB_polBkg_nominal",app.Data());
   results2tree(workdir.Data());
   
   // Background variations
-  workdir = Form("DataFits_%s2D_2CB_polBkg_minvRange",app.Data());
+  workdir = Form("DataFits_%s_2D_2CB_polBkg_minvRange",app.Data());
   results2tree(workdir.Data());
-  workdir = Form("DataFits_%s2D_2CB_polBkg_LLR25",app.Data());
+  workdir = Form("DataFits_%s_2D_2CB_polBkg_LLR25",app.Data());
   results2tree(workdir.Data());
-  workdir = Form("DataFits_%s2D_2CB_polBkg_LLR10",app.Data());
+  workdir = Form("DataFits_%s_2D_2CB_polBkg_LLR10",app.Data());
   results2tree(workdir.Data());
-  workdir = Form("DataFits_%s2D_2CB_expBkg",app.Data());
+  workdir = Form("DataFits_%s_2D_2CB_expBkg",app.Data());
   results2tree(workdir.Data());
   
   // Signal variations
-  workdir = Form("DataFits_%s2D_2CB_polBkg_constrained",app.Data());
+  workdir = Form("DataFits_%s_2D_2CB_polBkg_constrained",app.Data());
   results2tree(workdir.Data());
-  workdir = Form("DataFits_%s2D_CBG_polBkg",app.Data());
+  workdir = Form("DataFits_%s_2D_CBG_polBkg",app.Data());
   results2tree(workdir.Data());
   
   // CtauErr variations
-  workdir = Form("DataFits_%s2D_2CB_polBkg_ctauErrTot",app.Data());
+  workdir = Form("DataFits_%s_2D_2CB_polBkg_ctauErrTot",app.Data());
   results2tree(workdir.Data());
   
   // CtauTrue variations
-  workdir = Form("DataFits_%s2D_2CB_polBkg_ctauRecoTemp",app.Data());
+  workdir = Form("DataFits_%s_2D_2CB_polBkg_ctauRecoTemp",app.Data());
   results2tree(workdir.Data());
   
   // CtauRes variations
-  workdir = Form("DataFits_%s2D_2CB_polBkg_ctauResPromptMC",app.Data());
+  workdir = Form("DataFits_%s_2D_2CB_polBkg_ctauResPromptMC",app.Data());
   results2tree(workdir.Data());
-  workdir = Form("DataFits_%s2D_2CB_polBkg_ctauResNonPromptMC",app.Data());
+  workdir = Form("DataFits_%s_2D_2CB_polBkg_ctauResNonPromptMC",app.Data());
   results2tree(workdir.Data());
   
   // CtauBkg variations
-  workdir = Form("DataFits_%s2D_2CB_polBkg_ctauBkgTemp",app.Data());
+  workdir = Form("DataFits_%s_2D_2CB_polBkg_ctauBkgTemp",app.Data());
   results2tree(workdir.Data());
   
   return;
