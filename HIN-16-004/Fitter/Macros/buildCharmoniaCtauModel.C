7940db4940ffa4fd1e674911fd13f5cc639711b4
Andre Stahl <stahlleiton@gmail.com>
2017-06-21 02:09:17 +0200

    Moved to full RooKeysPdf algo for Bkg Template fits

--- HIN-16-004/Fitter/Macros/buildCharmoniaCtauModel.C
+++ HIN-16-004/Fitter/Macros/buildCharmoniaCtauModel.C
@@ -9,7 +9,7 @@ bool defineCtauResolModel(RooWorkspace& ws, string object, CtauModel model, map<
 bool addSignalCtauModel(RooWorkspace& ws, string object, CtauModel model, map<string,string> parIni, bool isPbPb); 
 bool addBackgroundCtauModel(RooWorkspace& ws, string object, CtauModel model, map<string,string> parIni, bool isPbPb);
 bool createCtauBkgTemplate(RooWorkspace& ws, string dsName, string pdfType, struct KinCuts cut, bool incJpsi, bool incPsi2S, double binWidth);
-bool ctauBkgHistToPdf(RooWorkspace& ws, TH1D* hist, string pdfName, vector<double> range);
+bool ctauBkgHistToPdf(RooWorkspace& ws, TH1D* hist, string pdfName, vector<double> range, bool useDataSet=true);
 TH1* rebinctauBkghist(RooWorkspace& ws, TH1 *hist, double xmin=1e99, double xmax=-1e99);
 void findRangeFromPlot(vector<double>& range, struct KinCuts cut, double binWidth);
 
@@ -1022,40 +1022,45 @@ bool createCtauBkgTemplate(RooWorkspace& ws, string dsName, string pdfType, stru
   return true;
 };
 
-bool ctauBkgHistToPdf(RooWorkspace& ws, TH1D* hist, string pdfName, vector<double> range)
+bool ctauBkgHistToPdf(RooWorkspace& ws, TH1D* hist, string pdfName, vector<double> range, bool useDataSet)
 {
   if (ws.pdf(pdfName.c_str())) {
     cout << Form("[INFO] The %s Template has already been created!", pdfName.c_str()) << endl;
     return true;
   }
-  // Cleaning the input histogram
-  // 1) Remove the Under and Overflow bins
-  hist->ClearUnderflowAndOverflow();
-  // 2) Set negative bin content to zero
-  for (int i=0; i<=hist->GetNbinsX(); i++) { if (hist->GetBinContent(i)<0) { hist->SetBinContent(i, 0.0000000001); } }
-  // 2) Reduce the range of histogram and rebin it
-  TH1* hClean = rebinctauBkghist(ws, hist, range[0], range[1]);
   
   cout << Form("[INFO] Implementing %s Template", pdfName.c_str()) << endl;
   
-  string dataName = pdfName;
-  dataName.replace(dataName.find("pdf"), string("pdf").length(), "dh");
-  RooDataHist* dataHist = new RooDataHist(dataName.c_str(), "", *ws.var("ctau"), hClean);
-  if (dataHist==NULL) { cout << "[ERROR] DataHist used to create " << pdfName << " failed!" << endl; return false; }
-  if (dataHist->sumEntries()==0) { cout << "[ERROR] DataHist used to create " << pdfName << " is empty!" << endl; return false; }
-  if (fabs(dataHist->sumEntries()-hClean->GetSumOfWeights())>0.001) { cout << "[ERROR] DataHist used to create " << pdfName << "  " << " is invalid!  " << endl; return false; }
-  ws.import(*dataHist);
-  
-  bool isPbPb=false;
-  if (pdfName.find("PbPb")!=std::string::npos) isPbPb=true;
-  RooHistPdf* pdf = new RooHistPdf(pdfName.c_str(), pdfName.c_str(), *ws.var("ctau"), *((RooDataHist*)ws.data(dataName.c_str())));
-  //RooKeysPdf* pdf = new RooKeysPdf(pdfName.c_str(), pdfName.c_str(), *ws.var("ctau"), *((RooDataSet*)ws.data(dataName.c_str())),RooKeysPdf::NoMirror, 0.4);
-  if (pdf==NULL) { cout << "[ERROR] RooKeysPDF " << pdfName << " is NULL!" << endl; return false; }
+  RooKeysPdf* pdf = NULL;
+  if (useDataSet) {
+    string dsName = "dOS_DATA_PbPb"; if (dataName.find("PP")!=string::npos) dsName = "dOS_DATA_PP";
+    if (ws.data(dsName.c_str())==NULL) { cout << "[ERROR] DataSet " << dsName << " for Bkg Template was not found!" << endl; return false; }
+    pdf = new RooKeysPdf(pdfName.c_str(), pdfName.c_str(), *ws.var("ctau"), *((RooDataSet*)ws.data(dsName.c_str())), RooKeysPdf::MirrorAsymBoth);
+  }
+  else {
+    // Cleaning the input histogram
+    // 1) Remove the Under and Overflow bins
+    hist->ClearUnderflowAndOverflow();
+    // 2) Set negative bin content to zero
+    for (int i=0; i<=hist->GetNbinsX(); i++) { if (hist->GetBinContent(i)<0) { hist->SetBinContent(i, 0.0000000001); } }
+    // 3) Reduce the range of histogram and rebin it
+    TH1* hClean = rebinctauBkghist(ws, hist, range[0], range[1]);
+    // 4) Create the RooDataHist for the Template
+    string dataName = pdfName;
+    dataName.replace(dataName.find("pdf"), string("pdf").length(), "dh");
+    RooDataHist* dataHist = new RooDataHist(dataName.c_str(), "", *ws.var("ctau"), hClean);
+    if (dataHist==NULL) { cout << "[ERROR] DataHist used to create " << pdfName << " failed!" << endl; return false; }
+    if (dataHist->sumEntries()==0) { cout << "[ERROR] DataHist used to create " << pdfName << " is empty!" << endl; return false; }
+    if (fabs(dataHist->sumEntries()-hClean->GetSumOfWeights())>0.001) { cout << "[ERROR] DataHist used to create " << pdfName << "  " << " is invalid!  " << endl; return false; }
+    ws.import(*dataHist);
+    // 4) Make the Template
+    pdf = new RooHistPdf(pdfName.c_str(), pdfName.c_str(), *ws.var("ctau"), *((RooDataHist*)ws.data(dataName.c_str())));
+    //RooKeysPdf* pdf = new RooKeysPdf(pdfName.c_str(), pdfName.c_str(), *ws.var("ctau"), *((RooDataSet*)ws.data(dataName.c_str())),RooKeysPdf::NoMirror, 0.4)
+    delete dataHist;
+  }
   pdf->setNormRange("CtauFullWindow");
   ws.import(*pdf);
-  
   delete pdf;
-  delete dataHist;
   
   return true;
 };
